0285d7a74fdb001871350efa2dab1b04499d7212
TanveerAli Shaikh <tanveeralishaikh@MacBook-Pro.fritz.box>
2023-06-29 23:43:08 +0200

    updated base_url

--- src/sdk/index.ts
+++ src/sdk/index.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://dataguard-server-56lu.vercel.app";
 
 export const getTabs = async (): Promise<any[]> => {
   const response = await fetch(`${BASE_URL}/tabs`);
